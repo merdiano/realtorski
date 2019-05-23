ddc3c6464f2e97056e23b2cbab78adc8a49e6ed1
merdiano <merdan.m@gmail.com>
2019-05-24 00:28:20 +0500

    routes api:auth middlware

--- routes/api.php
+++ routes/api.php
@@ -13,27 +13,31 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::post('userLogin', 'UserController@userLogin');
-Route::post('userRegister', 'UserController@userRegister');
+Route::post('/user/login', 'UserController@userLogin');
+Route::post('/user/register', 'UserController@userRegister');
+
 Route::group(['middleware' => 'auth:api'], function(){
-    Route::get('userDetails', 'UserController@userDetails');
+    Route::get('/user/details', 'UserController@userDetails');
+
+    Route::post('/estate/create', 'EstateController@store');
+    Route::get('/estate/delete/{id}', 'EstateController@delete');
+
+    Route::post('/vehicle/create', 'VehicleController@store');
+    Route::get('/vehicle/delete/{id}', 'VehicleController@delete');
+
+    Route::get('/announcement/delete/{id}', 'AnnouncementController@delete');
+    Route::post('/announcement/create', 'AnnouncementController@store');
 });
 
 Route::get('/location/{id?}', 'LocationController@index');
 Route::get('/category/{id?}', 'CategoryController@index');
 
 // /api/announcement/<main category id>/?subcategory=<3>&locationP=<location id>&locatinC
-Route::post('/estate/create', 'EstateController@store');
 Route::get('/estate/list', 'EstateController@list');
 Route::get('/estate/item/{id}', 'EstateController@item');
-Route::get('/estate/delete/{id}', 'EstateController@delete');
 
-Route::post('/vehicle/create', 'VehicleController@store');
 Route::get('/vehicle/list', 'VehicleController@list');
 Route::get('/vehicle/item/{id}', 'VehicleController@item');
-Route::get('/vehicle/delete/{id}', 'VehicleController@delete');
 
 Route::get('/announcement/list', 'AnnouncementController@list');
 Route::get('/announcement/item/{id}', 'AnnouncementController@item');
-Route::get('/announcement/delete/{id}', 'AnnouncementController@delete');
-Route::post('/announcement/create', 'AnnouncementController@store');
